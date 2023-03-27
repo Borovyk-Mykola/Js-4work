e849611fa7ccbdf938935b9c28461842dbc532e6
Borovyk-Mykola <114910601+Borovyk-Mykola@users.noreply.github.com>
2023-03-27 19:27:32 +0300

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+startBtn.addEventListener ('click', () => {
+    timerId = setInterval(() => {
+        body.style.backgroundColor =getRandomHexColor()
+    }, 1000);
+});
+
+stopBtn.addEventListener ('click', () => {
+    clearInterval(timerId);
+});
